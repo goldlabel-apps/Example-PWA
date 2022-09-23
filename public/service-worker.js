27397e45fb02f5ed97b46c10582636bf542a4085
listingslab <listingslab@gmail.com>
2022-09-23 06:46:23 +0200

    bumps version

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,10 +14,10 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v2.4.5';
+const PRECACHE = 'precache-v2.4.6';
 const RUNTIME = 'runtime';
 
-console.log("Dive Malta Service Worker 2.4.5");
+console.log("Dive Malta Service Worker 2.4.6");
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
