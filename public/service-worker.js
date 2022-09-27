1baa09ece5ce47eedad9196b96aa4175e312e2fd
listingslab <listingslab@gmail.com>
2022-09-27 18:08:40 +0200

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,33 +1,15 @@
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
-
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
-const PRECACHE = 'precache-v2.4.6';
+const PRECACHE = 'precache-v3.1.6b';
 const RUNTIME = 'runtime';
+console.log("DM Service Worker 3.1.6c");
 
-console.log("Dive Malta Service Worker 2.4.6");
-
-// A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   'index.html',
-  './', // Alias for index.html
+  './',
   'png/logo192.png',
 ];
 
-// The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
+  console.log("DM install");
   event.waitUntil(
     caches.open(PRECACHE)
       .then(cache => cache.addAll(PRECACHE_URLS))
@@ -35,9 +17,8 @@ self.addEventListener('install', event => {
   );
 });
 
-// The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', event => {
-  console.log("Dive Malta SW activated");
+  console.log("DM activate");
   const currentCaches = [PRECACHE, RUNTIME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
@@ -50,11 +31,8 @@ self.addEventListener('activate', event => {
   );
 });
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
 self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
+  console.log("DM fetch");
   if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
       caches.match(event.request).then(cachedResponse => {
@@ -63,7 +41,6 @@ self.addEventListener('fetch', event => {
         }
         return caches.open(RUNTIME).then(cache => {
           return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
             return cache.put(event.request, response.clone()).then(() => {
               return response;
             });
